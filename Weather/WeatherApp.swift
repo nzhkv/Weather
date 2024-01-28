9a596cc8aa9b1f0002756798b7b8ccae97bc9ba0
NZhukov <nzhukov.rnd@gmail.com>
2024-01-28 12:46:55 +0400

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Nikolay Zhukov on 28.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
