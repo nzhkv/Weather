23fb8024681836d08d5572b602baaaf45ce61f76
NZhukov <nzhukov.rnd@gmail.com>
2024-01-29 21:03:37 +0400

    add shapes

---
+++ Weather/Utils/Shapes.swift
@@ -0,0 +1,8 @@
+//
+//  Shapes.swift
+//  Weather
+//
+//  Created by Nikolay Zhukov on 29.01.2024.
+//
+
+import Foundation
---
+++ Weather/Views/Navigation/TabBar.swift
@@ -0,0 +1,18 @@
+//
+//  TabBar.swift
+//  Weather
+//
+//  Created by Nikolay Zhukov on 28.01.2024.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBar()
+}
